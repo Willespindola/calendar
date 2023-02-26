cdf02380c207e28d2fb7e062b8941ac3902128ff
Willian Silveira Espindola <87204387+Willespindola@users.noreply.github.com>
2023-02-26 01:09:33 -0300

    Add files via upload

---
+++ conection/pdo.php
@@ -0,0 +1,17 @@
+<?php
+global $conn;
+
+$servername = "localhost";
+$database = "agendaFull";
+$username = "root";
+$password = "";
+
+$sql = "mysql:host=$servername;dbname=$database;";
+$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+// Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
+try { 
+  $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
+  echo "Connected successfully";
+} catch (PDOException $error) {
+  echo 'Connection error: ' . $error->getMessage();
+}
